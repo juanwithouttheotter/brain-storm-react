6a8768a2d581b763e607934ed9dca5aca5c533a2
Juan Osorio <juan.michael.osorio@gmail.com>
2021-01-30 19:12:21 -0600

    creating first component placeholders

---
+++ src/components/CurriculumCard/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function CurriculumCard(props) {
+    return(
+        <div>
+            CurriculumCard
+        </div>
+    );
+}
+
+export default CurriculumCard;
