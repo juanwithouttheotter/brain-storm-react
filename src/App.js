e14601548f4bf1e083057992bbf16ac45f104ccc
Juan Osorio <juan.michael.osorio@gmail.com>
2021-01-30 21:08:30 -0600

    set basic template

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+import NewLesson from "./pages/NewLesson";
+import CurriculumOverview from "./pages/CurriculumOverview";
+import UserProfile from "./pages/UserProfile";
+
 
 function App() {
   return (
-    <div className="home">
-      Home
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/newlesson" exact={true} component={NewLesson} />
+        <Route path="/curriculumoverview" exact={true} component={CurriculumOverview} />
+        <Route path="/profile" exact={true} component={UserProfile} />
+      </Switch>
+    </Router>
   );
 }
 
